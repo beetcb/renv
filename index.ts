16cff57a6c2d71b62224b65ce92f73e553cbf21a
beetcb <63141491+beetcb@users.noreply.github.com>
2021-08-01 21:14:35 +0800

    init

---
+++ index.ts
@@ -0,0 +1,12 @@
+// TODO: find type defination for event Object
+addEventListener("fetch", (event: any) => {
+  const { PASS, LICENSE } = Deno.env.toObject();
+  const url = new URL(event.request.url);
+  const pass = url.searchParams.get("pass");
+  if (pass === PASS) {
+    const response = new Response(LICENSE, {
+      headers: { "content-type": "text/plain" },
+    });
+    event.respondWith(response);
+  }
+});
