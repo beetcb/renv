9c7309f2102917527796045863b75e54ac691ce6
beetcb <63141491+beetcb@users.noreply.github.com>
2021-08-01 22:43:11 +0800

    response with .env

--- index.ts
+++ index.ts
@@ -1,15 +1,28 @@
+import { EOL } from "https://deno.land/std@0.103.0/fs/mod.ts";
+
 addEventListener("fetch", (event: FetchEvent) => {
-  const { PASS, LICENSE, UNIDOC_LICENSE_API_KEY, APP_KEY, SIGN, WORKSHEET_ID } =
+  const { PASS, UNIDOC_LICENSE_API_KEY, APP_KEY, SIGN, WORKSHEET_ID } =
     Deno.env.toObject();
-  const url = new URL(event!.request.url);
+  const url = new URL(event.request.url);
   const pass = url.searchParams.get("pass");
+  const dotEnv = {
+    UNIDOC_LICENSE_API_KEY,
+    APP_KEY,
+    SIGN,
+    WORKSHEET_ID,
+  };
   if (pass === PASS) {
-    const response = new Response(LICENSE, {
-      headers: { "content-type": "text/plain" },
-    });
-    event!.respondWith(response);
+    const response = new Response(
+      Object.entries(dotEnv)
+        .map(([k, v]: [string, string]) => `${k}=${v}`)
+        .join(EOL.CRLF),
+      {
+        headers: { "content-type": "text/plain" },
+      }
+    );
+    event.respondWith(response);
   } else {
-    event!.respondWith(
+    event.respondWith(
       new Response(null, {
         status: 500,
       })
