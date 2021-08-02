cd70a81243945d9b26bd47cf9b2e969a2c7bbb3b
beetcb <63141491+beetcb@users.noreply.github.com>
2021-08-02 10:00:38 +0800

    ignore case

--- index.ts
+++ index.ts
@@ -2,18 +2,25 @@ import { EOL } from "https://deno.land/std@0.103.0/fs/mod.ts";
 
 addEventListener("fetch", (event: FetchEvent) => {
   const envObject = Deno.env.toObject();
-
   const { ENV_PASS } = envObject;
 
   const url = new URL(event.request.url);
   const pass = url.searchParams.get("pass");
-  const envName= url.pathname.replace(/$\//, '');
+  const envName = url.pathname.replace(/$\//, "");
 
   const dotEnv = Object.fromEntries(
     Object.keys(envObject).filter((k) => k.startsWith(envName)).map(
-      (k) => [k.replace(new RegExp(`^${envName}_`), ''), envObject[k]],
+      (k) => [k.replace(new RegExp(`^${envName}_`, "i"), ""), envObject[k]],
     ),
   );
+
+  console.log({
+    url,
+    pass,
+    envName,
+    dotEnv,
+    ENV_PASS,
+  });
 
   if (pass === ENV_PASS) {
     const response = new Response(
@@ -27,7 +34,7 @@ addEventListener("fetch", (event: FetchEvent) => {
     event.respondWith(response);
   } else {
     event.respondWith(
-      new Response(null, {
+      new Response("PASS Auth Failed", {
         status: 500,
       }),
     );
