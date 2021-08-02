59d7b6c3f386bfc8c7f49705f5b2189ad9c5eacf
beetcb <63141491+beetcb@users.noreply.github.com>
2021-08-02 09:22:11 +0800

    add env path setting

--- index.ts
+++ index.ts
@@ -1,31 +1,35 @@
 import { EOL } from "https://deno.land/std@0.103.0/fs/mod.ts";
 
 addEventListener("fetch", (event: FetchEvent) => {
-  const { PASS, UNIDOC_LICENSE_API_KEY, APP_KEY, SIGN, WORKSHEET_ID } =
-    Deno.env.toObject();
+  const envObject = Deno.env.toObject();
+
+  const { ENV_PASS } = envObject;
+
   const url = new URL(event.request.url);
   const pass = url.searchParams.get("pass");
-  const dotEnv = {
-    UNIDOC_LICENSE_API_KEY,
-    APP_KEY,
-    SIGN,
-    WORKSHEET_ID,
-  };
-  if (pass === PASS) {
+  const { pathname } = url;
+
+  const dotEnv = Object.fromEntries(
+    Object.keys(envObject).filter((k) => pathname.startsWith(`/${k}`)).map(
+      (k) => [k, envObject[k]],
+    ),
+  );
+
+  if (pass === ENV_PASS) {
     const response = new Response(
       Object.entries(dotEnv)
         .map(([k, v]: [string, string]) => `${k}=${v}`)
         .join(EOL.CRLF),
       {
         headers: { "content-type": "text/plain" },
-      }
+      },
     );
     event.respondWith(response);
   } else {
     event.respondWith(
       new Response(null, {
         status: 500,
-      })
+      }),
     );
   }
 });
